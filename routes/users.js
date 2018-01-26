cb23928a21e2c6387dd6b98cbce2e9ecb76c4e7d
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2018-01-26 23:20:06 +0000

    completed initial routes

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,39 @@ require('dotenv').config();
 
 module.exports = () => {
   router.get('/', (req, res) => {
-    console.log("here");
-    return res.send('ok');
+    // console.log("here");
+    return res.send('home page');
   })
+  
+  router.get('/friends/:uid', (req, res) => {
+    user_id = req.params.uid;
+    console.log(user_id);
+    return res.send('friends for user')
+  })
+
+  router.get('/trips/:uid', (req, res) => {
+    user_id = req.params['uid']
+    console.log(user_id);
+    return res.send('all trips for user');
+  })
+
+  router.get('/trips/:tid', (req, res) => {
+    trip_id = req.params.tid;
+    console.log(trip_id);
+    return res.send('individual trip')
+  })
+
+  router.get('/trips/:tid/activities', (req, res) => {
+    trip_id = req.params.tid;
+    console.log(trip_id);
+    return res.send('trip activities');
+  })
+
+  router.get('/activities/:aid/comments', (req, res) => {
+    trip_id = req.params.tid;
+    console.log(trip_id);
+    return res.send('comments for a specific activity');
+  })
+
   return router;
 }
