6e9d265ecf6e880e5a3e406261c02a3f7353dc10
Derrck Persson <derrickpersson@gmail.com>
2018-01-27 17:50:45 -0800

    Added city images object, mock API

---
+++ db/seeds/users_seeds.js
@@ -0,0 +1,64 @@
+const userData = [
+  {first_name: 'jim', last_name: 'bob', email: 'a@a.com', password: '1'},
+  {first_name: 'joe', last_name: 'bob', email: 'a2@a.com', password: '2'},
+  {first_name: 'john', last_name: 'bob', email: 'a3@a.com', password: '3'}
+]
+
+const tripsData = [
+  {name: 'First Friend Trip!', location: 'Seattle', owner: 'jim'},
+  {name: 'Second Friend Trip!', location: 'Portland', owner: 'jim'},
+  {name: 'Third Friend Trip!', location: 'San Diego', owner: 'jim'}
+]
+
+const usersTripsData = [
+  {trip: 'First Friend Trip!', user: 'joe'},
+  {trip: 'First Friend Trip!', user: 'john'}
+]
+
+
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users_trips').del()
+    .then(() => {
+      return knex('trips').del()
+    })
+    .then( () => {
+      return knex('users').del()
+    })
+    .then( () => {
+      return knex('users').insert(userData)
+    })
+    .then(() => {
+      let tripPromises = [];
+      tripsData.forEach((trip) => {
+        let owner = trip.owner;
+        tripPromises.push(
+          knex('users').where('first_name', owner).first()
+            .then( (userRecord) => {
+              return knex('trips').insert({
+                name: trip.name,
+                location: trip.location,
+                owner_id: userRecord.id
+              })
+            })
+          );
+      })
+      return Promise.all(tripPromises);
+    })
+    // .then(() => {
+    //   let usersTripsPromises = [];
+    //   usersTripsData.forEach((userTrip) => {
+    //     usersTripsPromises.push(
+    //       knex('users').where('first_name', userTrip.user).first()
+    //       .then( (userRecord) => {
+    //         return knex('users_trips').insert({
+    //           user_id: userRecord.id,
+    //           trip_id:
+    //         })
+    //       })
+    //       )
+    //   })
+
+    // })
+};
---
+++ routes/cityImages.js
@@ -0,0 +1,37 @@
+// const Flickr = require('flickr-sdk')
+
+// const flickr = new Flickr("c2b4e258b6f0175ae0604b61c7186c42")
+
+// flickr.photos.search({
+//   text: "vancouver, landscape",
+//   url_o: true
+// }).then(function(res) {
+//   console.log('Yay!', res.body.photos.photo[0]);
+//   const photoInfo = res.body.photos.photo[0];
+//   console.log(`https://www.flickr.com/photos/${photoInfo.owner}/${photoInfo.id}`)
+//   console.log(`https://farm${photoInfo.farm}.staticflickr.com/${photoInfo.server}/${photoInfo.id}_${photoInfo.secret}_m.jpg`)
+// }).catch(function(err){
+//   console.error('Damn', err);
+// });
+// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+// https://flic.kr/p/EbNDvH
+// https://www.flickr.com/photos/118719274@N07/25063823487/in/photolist-EbNDvH
+
+
+const cityImages = {
+  "Vancouver": "https://images.pexels.com/photos/63332/science-world-false-creek-vancouver-british-columbia-63332.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+  "Seattle": "https://static.pexels.com/photos/656195/pexels-photo-656195.jpeg",
+  "San Fransisco": "https://static.pexels.com/photos/7653/pexels-photo.jpeg",
+  "New York": "https://static.pexels.com/photos/450597/pexels-photo-450597.jpeg",
+  "Portland": "https://static.pexels.com/photos/710908/pexels-photo-710908.jpeg",
+  "Boston": "https://images.pexels.com/photos/275606/pexels-photo-275606.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+  "Paris": "https://static.pexels.com/photos/338515/pexels-photo-338515.jpeg",
+  "London": "https://static.pexels.com/photos/50632/pexels-photo-50632.jpeg",
+  "Berlin": "https://static.pexels.com/photos/109630/pexels-photo-109630.jpeg",
+  "Cape Town": "https://static.pexels.com/photos/259447/pexels-photo-259447.jpeg",
+  "Cancun": "https://www.pexels.com/photo/beach-beautiful-bridge-carribean-449627/",
+  "Seoul": "https://static.pexels.com/photos/237211/pexels-photo-237211.jpeg",
+  "Tokyo":"https://static.pexels.com/photos/34142/pexels-photo.jpg"
+}
+
+console.log(cityImages["Vancouver"]);
