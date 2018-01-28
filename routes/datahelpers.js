35baf890c16fb5d6a2b0ba662af5d598e67096bb
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2018-01-28 01:02:21 +0000

    updated post routes in datahelpers

--- routes/datahelpers.js
+++ routes/datahelpers.js
@@ -5,9 +5,28 @@ module.exports = function makeDataHelpers(db) {
       return db.select('*').from('trips').where('owner_id', userid);
     },
 
+    //create a new trip for a given user
+    addTrip: function(tripinfo){
+      return db('trips').insert(tripinfo);
+    },
+
     // Select a single trip
     queryTrip: function(tripid){
       return db.select('*').from('trips').where('id', tripid);
+    },
+
+    // update a trip
+    updateTrip: function(tripinfo){
+      return db('trips').where('id', '=', tripinfo.id)
+      .update({
+        name: tripinfo.name,
+        location: tripinfo.location
+      });
+    },
+
+    // delete a trip
+    deleteTrip: function(tripid){
+      return db('trips').where({ id: tripid }).del();
     },
 
     // Get activities within a trip
@@ -15,10 +34,18 @@ module.exports = function makeDataHelpers(db) {
       return db.select('*').from('activities').where('trip_id', tripid);
     },
 
+    // add activities within a trip
+    addActivities: function(activityinfo) {
+      return db('activities').insert(activityinfo);
+    },
+
     // Get comments from an activity
     getComments: function(activityid){
       return db.select("*").from('comments').where('activity_id', activityid);
     },
 
+    postComments: function(commentinfo) {
+      return db('comments').insert(commentinfo);
+    }
   }
 }
